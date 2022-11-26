29b6fec1c03bd6e5349dd1bc16a2277ff5cab1d9
Gabriel Moreira <joaogmrodrigues@outlook.com>
2022-11-26 10:27:48 -0300

    ➕ add config backend

--- terraform/env/dev/main.tf
+++ terraform/env/dev/main.tf
@@ -4,6 +4,14 @@ module "aws-dev" {
   regiao_aws = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "gabu-terraform-bucket-mack-us-east-1"
+    key    = "tf/terraform.ftstate"
+    region = "us-east-1"
+  }
+}
+
 output "IP" {
   value = module.aws-dev.IP_Publico 
 }
